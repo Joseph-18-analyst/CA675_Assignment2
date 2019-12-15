4842f2bd76fbfd0133dd75b14d129a2076b64ac9
him89088 <vashisht.him@gmail.com>
2019-12-15 18:36:09 +0000

    Final commit

---
+++ wines.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   return render_template("Wine_Search.html")
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0')
